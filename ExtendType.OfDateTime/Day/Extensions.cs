27ff476487818dab30c78ffb384005739437a763
abstractlabs <abstractlabs@gmail.com>
2014-07-28 20:47:48 -0600

    Fixed naming of day extensions

--- ExtendType.OfDateTime/Day/Extensions.cs
+++ ExtendType.OfDateTime/Day/Extensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtendType.OfDateTime.Manipulation
+namespace ExtendType.OfDateTime.Day
 {
 	public static class Extensions
 	{
