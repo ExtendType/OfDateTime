0abec065b4f01506a2feb5437246189624826300
abstractlabs <abstractlabs@gmail.com>
2014-07-28 19:46:27 -0600

    Initial add

---
+++ ExtendType.OfDateTime/Day/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendType.OfDateTime.Manipulation
+{
+	public static class Extensions
+	{
+		public static DateTime StartOfDay(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, instance.Day, 0, 0, 0);
+		}
+
+		public static DateTime EndOfDay(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, instance.Day, 23, 59, 59, 999);
+		}
+	}
+}
