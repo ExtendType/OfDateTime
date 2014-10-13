afbe222884ae7ce66e93858f49638a4ea0da4654
abstractlabs <abstractlabs@gmail.com>
2014-10-13 17:13:28 -0600

    renamed day extension added difference and period extensions1

---
+++ ExtendType.OfDateTime/Difference/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendType.OfDateTime.Difference
+{
+	public static class Extensions
+	{
+		public static TimeSpan Until(this DateTime instance, DateTime date)
+		{
+			return new TimeSpan(date.Ticks - instance.Ticks);
+		}
+
+		public static int DaysUntil(this DateTime instance, DateTime date)
+		{
+			return Convert.ToInt32(Math.Ceiling(new TimeSpan(date.Ticks - instance.Ticks).TotalDays));
+		}
+		public static int WeeksUntil(this DateTime instance, DateTime date)
+		{
+			int result = 0;
+			double days = new TimeSpan(date.Ticks - instance.Ticks).TotalDays;
+			if (days != 0) result = Convert.ToInt32(Math.Floor(days / 7));
+			return result;
+		}
+		public static int MonthsUntil(this DateTime instance, DateTime date)
+		{
+			return ((instance.Year - date.Year) * 12) + (instance.Month - date.Month);
+		}
+
+		public static int QuartersUntil(this DateTime instance, DateTime date)
+		{
+			int result = ((instance.Year - date.Year) * 4);
+			int months = (instance.Month - date.Month);
+			if(months != 0) result = result + Convert.ToInt32(Math.Floor(Convert.ToDouble((months / 3))));
+			return result;
+		}
+
+		public static int YearsUntil(this DateTime instance, DateTime date)
+		{
+			return (instance.Year - date.Year);
+		}
+	}
+}
---
+++ ExtendType.OfDateTime/Periods/Extensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendType.OfDateTime.Periods
+{
+	public static class Extensions
+	{
+		public static DateTime StartOfDay(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, instance.Day, 0, 0, 0, 0);
+		}
+
+		public static DateTime EndOfDay(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, instance.Day, 23, 59, 59, 999);
+		}
+		public static DateTime StartOfMonth(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, 1, 0, 0, 0, 0);
+		}
+		public static DateTime EndOfMonth(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, DateTime.DaysInMonth(instance.Year, instance.Month), 23, 59, 59, 999);
+		}
+		public static DateTime StartOfWeek(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, Convert.ToInt32(instance.DayOfWeek) - instance.Day, 0, 0, 0, 0);
+		}
+		public static DateTime EndOfWeek(this DateTime instance)
+		{
+			return new DateTime(instance.Year, instance.Month, (Convert.ToInt32(instance.DayOfWeek) - instance.Day) + 6, 23, 59, 59, 999);
+		}
+		public static DateTime StartOfQuarter(this DateTime instance)
+		{
+			int month = instance.Month;
+			if (month < 4) month = 1;
+			else if (month < 6) month = 4;
+			else if (month < 8) month = 6;
+			else if (month <= 12) month = 8;
+			return new DateTime(instance.Year, month, 1, 0, 0, 0, 0);
+		}
+		public static DateTime EndOfQuarter(this DateTime instance)
+		{
+			int month = instance.Month;
+			if (month < 4) month = 3;
+			else if (month < 6) month = 7;
+			else if (month < 8) month = 9;
+			else if (month <= 12) month = 12;
+			return new DateTime(instance.Year, month, DateTime.DaysInMonth(instance.Year, month), 23, 59, 59, 999);
+		}
+
+		public static DateTime StartOfYear(this DateTime instance)
+		{
+			return new DateTime(instance.Year, 1, 1, 0, 0, 0, 0);
+		}
+		public static DateTime EndOfYear(this DateTime instance)
+		{
+			return new DateTime(instance.Year, 12, DateTime.DaysInMonth(instance.Year, 12), 23, 59, 59, 999);
+		}
+	}
+}
